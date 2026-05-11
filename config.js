20ee18c559ff7fa071218fec07d4010ac2d26050
Vexla <microsoftkeziah@gmail.com>
2026-05-11 13:02:43 +0200

    config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VEXLA_CONFIG = {
-  BACKEND_BASE_URL: "https://backend-render.onrender.com",
+  BACKEND_BASE_URL: "https://backhend-render.onrender.com"
   PREVIEW_ENDPOINT: "/preview-business",
   CHECKOUT_ENDPOINT: "/create-checkout-session",
   VERIFY_ENDPOINT: "/verify-session"
